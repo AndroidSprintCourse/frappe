1e3a515a75c47d16bb912a7bd8a3db6eb427cf71
Fridon <fedin.mail@gmail.com>
2025-01-10 23:21:59 +0300

    Update me.html
    fix: bulk_insert child tables malformed (#26876)
    
    Generator within generator caused something funky to happen which meant wrong values being stitched together as one document, sometimes(?)
    
    Via https://github.com/frappe/frappe/pull/22114
    
    adding cron job
    
    Adding field telegram username
    
    Adding counter of completed lessons
    
    Adding to navbar subsciptions and tariffs
    
    Fixing navbar

--- frappe/www/me.py
+++ frappe/www/me.py
@@ -12,5 +12,13 @@ def get_context(context):
 	if frappe.session.user == "Guest":
 		frappe.throw(_("You need to be logged in to access this page"), frappe.PermissionError)
 
+	lessons_completed = frappe.get_all(
+        "LMS Course Progress",
+        filters={"member": frappe.session.user, "status": "Complete"},
+        # fields=["lesson"],
+    )
+	lessons_count = frappe.get_all("Course Lesson", fields=["name"])
+	context.lessons_count = len(lessons_count)
+	context.lessons_completed = len(lessons_completed)
 	context.current_user = frappe.get_doc("User", frappe.session.user)
 	context.show_sidebar = False
